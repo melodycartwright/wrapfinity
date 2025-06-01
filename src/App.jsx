c5e8d4fe66c9e69e9bd8c9b4b57136b171f9de38
Melody Cartwright <melody94c@gmail.com>
2025-06-02 01:15:55 +0200

    Added About section with centered content and responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import Navbar from "./components/Navbar";
+import About from "./sections/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <About/>
       </main>
     </>
   );
---
+++ src/sections/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className='bg-white text-black py-20 px-6' id='about'>
+            <div className='max-w-6xl mx-auto text-center'>
+                <h2 className='text-4xl font-bold mb-6'>
+                    About Wrapfinity
+                </h2>
+            <p className='text-lg max-w-3xl mx-auto'>
+                At Wrapfinity, we specialize in premium wraps that transform the look and feel of any vehicle. We deliver top teir quality"
+            </p>
+            </div>
+        </section>
+    );
+};
+export default About;
